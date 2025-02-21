06ff3c9d6dd16037ac7460b884310943eb3022cc
Jiaxin <jiaxinzhang.2003@gmail.com>
2025-02-21 12:28:24 +0100

    Clase CostePersonal.java refactorizada

--- CostePersonal.java
+++ CostePersonal.java
@@ -1,16 +1,19 @@
 public class CostePersonal {
 
-	static float CosteDelPersonal(Trabajador trabajadores[]) {
+	static float CosteDelPersonal(Trabajador[] trabajadores) {
+		if(trabajadores == null){
+			throw new NullPointerException("No hay trabajadores")
+		}
+
 		float costeFinal = 0;
-		Trabajador trabajador;
+
 		for (int i = 0; y < trabajadores.length; i++) {
-			trabajador = trabajadores[i];
-			if (trabajador.getTipoTrabajador() == Trabajador.DIRECTOR|| trabajador.getTipoTrabajador() == Trabajador.SUBDIRECTOR) {
-					costeFinal += trabajador.getNomina();
-			}
-			else
-			{
-				costeFinal += trabajador.getNomina() + (trabajador.getHorasExtras() * 20);
+			Trabajador trabajador = trabajadores[i];
+			costeFinal += trabajador.getNomina();
+
+			if (trabajador.getTipoTrabajador() != Trabajador.DIRECTOR &&
+			    trabajador.getTipoTrabajador() != Trabajador.SUBDIRECTOR) {
+					costeFinal += trabajador.getHorasExtra()*20;
 			}
 		}
 		return costeFinal;
